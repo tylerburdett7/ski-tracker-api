6cbc0cbc998432b1bebe765506cc89cfde51267b
Tyler Burdett <bur23034@byui.edu>
2025-11-07 16:43:41 -0700

    fixed function name in authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
-export function ensureAuthenticated(req, res, next) {
+export function checkAuth(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.status(401).json({ message: "Unauthorized: Please log in with Google" });
+  res.status(401).json({ message: "You must be logged in to perform this action" });
 }
